5544a4dbdcbba02d79edf336ddd719750380f897
ling_lai <ling_lai@compal.com>
2021-04-20 14:08:57 +0800

    add, help function button

--- js/SideBarBtn.js
+++ js/SideBarBtn.js
@@ -3,4 +3,10 @@ $(function(){
     document.getElementById('backBtn').addEventListener("click", function(e){
         location.reload();
     }, false);
+    document.getElementById('hintBtn').addEventListener("click", function(e){
+        $('.hint_container').removeClass('hidden');
+    }, false);
+    document.getElementById('closeHint').addEventListener("click", function(e){
+        $('.hint_container').addClass('hidden');
+    }, false);
 });
