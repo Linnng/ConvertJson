62cb0b33fa89a1be685194ceab53ab3f9f996592
ling_lai <ling_lai@compal.com>
2021-07-16 10:44:07 +0800

    completed HTML, sensor description, Plx throttle, Gfx throttle

--- js/btnControl_PlxThrottle.js
+++ js/btnControl_PlxThrottle.js
@@ -1,7 +1,7 @@
 
 $(function(){
 
-    $('#SensorDesc_AddSku_Btn').click(function(e){
+    $('#PlxThrottle_AddSku_Btn').click(function(e){
 
         var sensorSkuContainer = $("<div/>", {"class": "sensor_sku_container"});
 
@@ -41,21 +41,12 @@ $(function(){
 
         // new sensor description
         var scrollBoxDiv = $("<div/>", {"class": "horizon_scroll_box"});
-        var sensorTab    = $("<table/>", {"id": "sensorCommonTab", "class": "sensor_tab"});
+        var sensorTab    = $("<table/>", {"class": "sensor_tab"});
 
         var sensorTitPlaceholder =
             [
                 'name',
-                'type',
-                'address',
-                'diagclass',
-                'polling ratae',
-                'ReadFn',
-                'MaxErrors',
-                'SmoothingFn',
-                'SmoothingData',
-                'CriticalTemp',
-                'crittime'
+                'Throttle Point'
             ];
 
         for(var i=0; i<3; i++)
@@ -123,142 +114,10 @@ $(function(){
         scrollBoxDiv.append(sensorTab);
         sensorSkuContainer.append(scrollBoxDiv);
 
-        $('#sensorContainer').append(sensorSkuContainer);
+        $('#plxContainer').append(sensorSkuContainer);
 
         // reset table width
         var sensorTabCnt = $(e.target).parents('.container').find('.sensor_sku_container:last').index('.sensor_sku_container');
         SetSensorComTabWid(sensorTabCnt);
-
     });
-
-
-    // sensor discrete switch
-    // ===========================================================
-    // $('#discrete_switch').click(function(){
-    //     if($(this).prop("checked"))
-    //         showSensorDis();
-    //     else
-    //         removeSensorDis();
-    // });
-    // ===========================================================
-
 });
-
-
-
-function removeSku(e){
-    $(e).parents('.sensor_sku_container').remove();
-}
-
-
-
-// "add sensor" button: add table row
-function AddSensorRow(e){
-
-    var sensorTr = $("<tr/>", {"class": "sensor_tr"});
-    var ThisSensorTab = $(e).parents('.sensor_sku_container').find('.sensor_tab');            // Get this table.sensor_tab
-    var sensorThCnt   = $(e).parents('.sensor_sku_container').find('.sensor_tab th').length;  // count table th length
-
-
-    for(var i=0; i<sensorThCnt; i++){
-
-        var sensorTd = $("<td/>");
-        var sensorOpt =
-            [
-                {'value':'EXEC_POLLING',   'text':'1/8 sec'},
-                {'value':'QSEC_POLLING',   'text':'1/4 sec'},
-                {'value':'HSEC_POLLING',   'text':'1/2 sec'},
-                {'value':'ONESEC_POLLING', 'text':'1 sec'  },
-                {'value':'TWOSEC_POLLING', 'text':'2 sec'  }
-            ];
-
-        if( ThisSensorTab.find('tr th:eq(' + i + ')') != undefined &&
-            ThisSensorTab.find('tr th:eq(' + i + ')').text().trim() == 'btn')
-        {
-            var deleteSpan = $("<span/>").text('Delete');
-            var deleteDiv  = $("<div/>", {"class": "sensorDelete", "onclick": "removeRow(this)"}).append(deleteSpan);
-            sensorTd.append(deleteDiv);
-        }
-
-        else if( ThisSensorTab.find('tr th:eq(' + i + ')') != undefined &&
-                 ThisSensorTab.find('tr th:eq(' + i + ')') == 'polling rate' )
-        {
-            var sensorSel = $("<select/>", {"name": "pollingrate", "class": "selects"});
-
-            for(var j=0; j<sensorOpt.length ;j++)
-            {
-                sensorSel.append("<option value='" + sensorOpt[j].value + "'>" + sensorOpt[j].text + "</option>");
-                sensorTd.append(sensorSel);
-            }
-        }
-
-        else
-        {
-            sensorTd.append('<input/>');
-        }
-        sensorTr.append(sensorTd);
-    }
-    ThisSensorTab.append(sensorTr);
-}
-
-
-
-// "add type" button: add table column
-function  AddSensorColumn(e){
-
-    var ThisSensorTab = $(e).parents('.sensor_sku_container').find('.sensor_tab');            // Get this table.sensor_tab
-
-    ThisSensorTab.find('tr').each(function(index){
-
-        var sensorTr = $(this);
-        if(index == 0)
-        {
-            var deleteSpan = $("<span/>").text('Delete');
-            var deleteDiv  = $("<div/>", {"class": "sensorDelete", "onclick": "removeColumn(this)"}).append(deleteSpan);
-            var sensorTd   = $('<td/>').append(deleteDiv);
-        }
-        else if(index == 1)
-        {
-            var sensorInput = $('<input/>', {"type": "test"});
-            var sensorTd = $('<th/>').append(sensorInput);  // <th>
-        }
-        else
-        {
-            var sensorInput = $('<input/>', {"type": "test"});
-            var sensorTd = $('<td/>').append(sensorInput);
-        }
-        sensorTr.append(sensorTd);
-    });
-
-    // reset table width
-    var sensorTabCnt = $(e).parents('.sensor_sku_container').index('.sensor_sku_container');
-    SetSensorComTabWid(sensorTabCnt);
-}
-
-
-
-// sensor delete button - row
-function removeRow(e){
-    $(e).parents('.sensor_tr').remove();
-}
-
-
-
-// sensor delete button - column
-function removeColumn(e){
-    // $(e).parents('.sensor_sku_container').css( "outline", "3px solid red" ); // okay
-
-    var deleteIndex  = $(e).parents().index();
-    var sensorTabCnt = $(e).parents('.sensor_sku_container').index('.sensor_sku_container');
-
-    $(e).parents('table').find('tr').each(function(){
-
-        if( $(this).find('td:eq('+deleteIndex+')').html() != undefined)
-            $(this).find('td:eq('+deleteIndex+')').remove();
-        else
-            $(this).find('th:eq('+deleteIndex+')').remove();
-    });
-
-    // reset table width
-    SetSensorComTabWid(sensorTabCnt);
-}
--- js/winSetting.js
+++ js/winSetting.js
@@ -1,148 +1,59 @@
 
 // Sensor description table width setting.
-function SetSensorComTabWid()
+function SetSensorComTabWid(index)
 {
-    var tableWid = 0;
+    if(index == -1)
+    {
+        // $('.sensor_tab:eq(' + index + ')').css( "outline", "3px solid blue" );
+        $('.sensor_tab').each(function(){
+            var tableWid = 0;
+            $(this).find('.sensor_tr th').each(function(index_wid){
+                if(index_wid == 0)
+                {
+                    $(this).width('80px');
+                    tableWid += 80;
+                }
+                else if(index_wid == 1)
+                {
+                    $(this).width('100px');
+                    tableWid += 100;
+                }
+                else{
+                    $(this).width('200px');
+                    tableWid += 200;
+                }
+            });
+            $(this).width(tableWid + 'px');
+        });
+    }
+    else
+    {
+        var tableWid = 0;
 
-    $('#sensorCommonTab .sensor_tr th').each(function(index){
-        if(index == 0)
-        {
-            $(this).width('80px');
-            tableWid += 80;
-        }
-        else if(index == 1)
-        {
-            $(this).width('100px');
-            tableWid += 100;
-        }
-        else
-        {
-            $(this).width('200px');
-            tableWid += 200;
-        }
-    });
+        $('.sensor_tab:eq(' + index + ')').find('.sensor_tr th').each(function(index_wid){
 
-    $('#sensorCommonTab').width( tableWid + 'px' );
-}
+            if(index_wid == 0)
+            {
+                $(this).width('80px');
+                tableWid += 80;
+            }
+            else if(index_wid == 1)
+            {
+                $(this).width('100px');
+                tableWid += 100;
+            }
+            else{
+                $(this).width('200px');
+                tableWid += 200;
+            }
+        });
 
-function SetSensorDisTabWid()
-{
-    var tableWid = 0;
-
-    $('.sensor_dis_tr th').each(function(index){
-        if(index == 0)
-        {
-            $(this).width('100px');
-            tableWid += 100;
-        }
-        else
-        {
-            $(this).width('200px');
-            tableWid += 200;
-        }
-    });
-
-    $('#sensorDiscreteTab').width( tableWid + 'px' );
-}
-
-
-
-// fan description table width setting.
-function SetFanInitTabWid()
-{
-    var tableWid = 0;
-
-    $('.fan_init_tr th').each(function(){
-        $(this).width('200px');
-        tableWid += 200;
-    });
-
-    $('.fan_init_tab').width( tableWid + 'px' );
-}
-
-function SetFanDesTabWid()
-{
-    var tableWid = 0;
-
-    $('.fan_desc_tab').find('tr:eq(1) th').each(function(){
-        $(this).width('125px');
-        tableWid += 125;
-    });
-
-    $('.fan_desc_tab').width( tableWid + 'px' );
-}
-
-
-
-// Plx throttle table width setting
-function SetPlxTabWid(){
-    var tableWid    = 0;
-    var tableDisWid = 0;
-
-    $('.plx_throttle_tab th').each(function(index){
-        if(index == 0)
-        {
-            $(this).width('80px');
-            tableWid += 80;
-        }
-        else
-        {
-            $(this).width('200px');
-            tableWid += 200;
-        }
-    });
-
-    $('.plx_throttle_dis_tab th').each(function(index){
-            $(this).width('240px');
-            tableDisWid += 240;
-    });
-
-    $('.plx_throttle_tab').width( tableWid + 'px' );
-    $('.plx_throttle_dis_tab').width( tableDisWid + 'px' );
-}
-
-
-function SetGfxComTabWid(){
-    var tableWid    = 0;
-
-    $('.gfx_throttle_tab th').each(function(index){
-        if(index == 0)
-        {
-            $(this).width('80px');
-            tableWid += 80;
-        }
-        else
-        {
-            $(this).width('125px');
-            tableWid += 125;
-        }
-    });
-
-    $('.gfx_throttle_tab').width( tableWid + 'px' );
-}
-
-function SetGfxDisTabWid(){
-    var tableWid    = 0;
-
-    $('.gfx_discrete_throttle_tab th').each(function(){
-        $(this).width('125px');
-        tableWid += 125;
-    });
-
-    $('.gfx_discrete_throttle_tab').width( tableWid + 'px' );
+        $('.sensor_tab:eq(' + index + ')').width(tableWid + 'px');
+    }
 }
 
 
 function HandleTableWid()
 {
-    SetSensorComTabWid();
-    SetSensorDisTabWid();
-
-    SetFanInitTabWid();
-    SetFanDesTabWid();
-
-    SetPlxTabWid();
-
-    SetGfxComTabWid();
-    SetGfxDisTabWid();
+    SetSensorComTabWid(-1);
 }
